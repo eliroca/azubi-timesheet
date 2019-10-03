d7fcc09ad740f55835798dd06cc1549ee9100a76
Elisei Roca <eroca@suse.de>
2019-10-03 12:12:06 +0200

    added main function, minor changes to help info

--- azubi-timesheet.py
+++ azubi-timesheet.py
@@ -9,7 +9,7 @@ Export and print at the end of the month!
 import sys
 import argparse
 
-def parse_cli(cliargs=None):
+def parse_cli(args=None):
     """Parse CLI with :class:`argparse.ArgumentParser` and return parsed result.
 
     :param list cliargs: Arguments to parse or None (=use sys.argv)
@@ -19,25 +19,37 @@ def parse_cli(cliargs=None):
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("-d", "--date",
                         dest="date",
-                        help="Date of record in format 'DD.MM.YY'.",
+                        help="date of record in format 'DD.MM.YY'",
                         )
     parser.add_argument("-w", "--work-hours",
                         dest="work_hours",
-                        help="Begin and end of work day in format 'HH:MM-HH:MM'.",
+                        help="begin and end of work day in format 'HH:MM-HH:MM'",
                         )
     parser.add_argument("-b", "--break-time",
                         dest="break_time",
-                        help="Begin and end of break in format 'HH:MM-HH:MM'.",
+                        help="begin and end of break in format 'HH:MM-HH:MM'",
                         )
     parser.add_argument("-c", "--comment",
                         dest="comment",
-                        help="Comment for record, if needed.",
+                        help="comment of record, if needed",
                         )
-    args = parser.parse_args(args=cliargs)
-    #check_args(args)
+    args = parser.parse_args(args)
+    #check_args(args) # TODO: implement checks
+    args.parser = parser
+    # If no argument is given, print help info:
+    if not sys.argv[1:]:
+        parser.print_help()
+        sys.exit(0)
     return args
 
-if __name__ == "__main__":
-    args = parse_cli()
+def main(args=None):
+    """main function of the script
+
+    :param list args: a list of arguments (sys.args[:1])
+    """
+    args = parse_cli(args)
     print("{}, {}, {}, {}".format(args.date, args.work_hours, args.break_time, args.comment))
-    sys.exit(0)
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
